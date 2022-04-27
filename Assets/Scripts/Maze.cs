433ce3fa45099c07dad8cbc9e950eff584828210
Sterlios <kuzmin.s.anton@gmail.com>
2022-04-27 23:48:10 +0200

    Random Cell Generation

--- Assets/Scripts/Maze.cs
+++ Assets/Scripts/Maze.cs
@@ -8,15 +8,19 @@ public class Maze : MonoBehaviour
 
 	private MazeCell[,] cells;
 
+	public MazeCell GetCell(IntVector2 coordinates)
+	{
+		return cells[coordinates.x, coordinates.z];
+	}
+
 	public void Generate()
 	{
 		cells = new MazeCell[size.x, size.z];
-		for (int x = 0; x < size.x; x++)
+		IntVector2 coordinates = RandomCoordinates;
+		while (ContainsCoordinates(coordinates) && GetCell(coordinates) == null)
 		{
-			for (int z = 0; z < size.z; z++)
-			{
-				CreateCell(new IntVector2(x, z));
-			}
+			CreateCell(coordinates);
+			coordinates += MazeDirections.RandomValue.ToIntVector2();
 		}
 	}
 
@@ -28,5 +32,17 @@ public class Maze : MonoBehaviour
 		newCell.name = "Maze Cell " + coordinates.x + ", " + coordinates.z;
 		newCell.transform.parent = transform;
 		newCell.transform.localPosition = new Vector3(coordinates.x - size.x * 0.5f + 0.5f, 0f, coordinates.z - size.z * 0.5f + 0.5f);
+	}
+	public IntVector2 RandomCoordinates
+	{
+		get
+		{
+			return new IntVector2(Random.Range(0, size.x), Random.Range(0, size.z));
+		}
+	}
+
+	public bool ContainsCoordinates(IntVector2 coordinate)
+	{
+		return coordinate.x >= 0 && coordinate.x < size.x && coordinate.z >= 0 && coordinate.z < size.z;
 	}
 }
