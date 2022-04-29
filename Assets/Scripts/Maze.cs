dd8661efc9aaea584570a2d5f1d4d3d5934c50d7
Sterlios <kuzmin.s.anton@gmail.com>
2022-04-29 16:16:47 +0200

    Update Maze.cs

--- Assets/Scripts/Maze.cs
+++ Assets/Scripts/Maze.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class Maze : MonoBehaviour
 {
@@ -16,15 +18,15 @@ public class Maze : MonoBehaviour
 	public void Generate()
 	{
 		cells = new MazeCell[size.x, size.z];
-		IntVector2 coordinates = RandomCoordinates;
-		while (ContainsCoordinates(coordinates) && GetCell(coordinates) == null)
+		List<MazeCell> activeCells = new List<MazeCell>();
+		DoFirstGenerationStep(activeCells);
+		while (activeCells.Count > 0)
 		{
-			CreateCell(coordinates);
-			coordinates += MazeDirections.RandomValue.ToIntVector2();
+			DoNextGenerationStep(activeCells);
 		}
 	}
 
-	private void CreateCell(IntVector2 coordinates)
+	private MazeCell CreateCell(IntVector2 coordinates)
 	{
 		MazeCell newCell = Instantiate(cellPrefab) as MazeCell;
 		cells[coordinates.x, coordinates.z] = newCell;
@@ -32,6 +34,7 @@ public class Maze : MonoBehaviour
 		newCell.name = "Maze Cell " + coordinates.x + ", " + coordinates.z;
 		newCell.transform.parent = transform;
 		newCell.transform.localPosition = new Vector3(coordinates.x - size.x * 0.5f + 0.5f, 0f, coordinates.z - size.z * 0.5f + 0.5f);
+		return newCell;
 	}
 	public IntVector2 RandomCoordinates
 	{
@@ -44,5 +47,26 @@ public class Maze : MonoBehaviour
 	public bool ContainsCoordinates(IntVector2 coordinate)
 	{
 		return coordinate.x >= 0 && coordinate.x < size.x && coordinate.z >= 0 && coordinate.z < size.z;
+	}
+
+	private void DoFirstGenerationStep(List<MazeCell> activeCells)
+	{
+		activeCells.Add(CreateCell(RandomCoordinates));
+	}
+
+	private void DoNextGenerationStep(List<MazeCell> activeCells)
+	{
+		int currentIndex = activeCells.Count - 1;
+		MazeCell currentCell = activeCells[currentIndex];
+		MazeDirection direction = MazeDirections.RandomValue;
+		IntVector2 coordinates = currentCell.coordinates + direction.ToIntVector2();
+		if (ContainsCoordinates(coordinates) && GetCell(coordinates) == null)
+		{
+			activeCells.Add(CreateCell(coordinates));
+		}
+		else
+		{
+			activeCells.RemoveAt(currentIndex);
+		}
 	}
 }
